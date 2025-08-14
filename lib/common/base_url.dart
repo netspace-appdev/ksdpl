b790785d32e33654985b14d41397fc179c50e520
Pankaj Devda <pankaj.devda@netspacesoftware.com>
2025-08-14 14:30:56 +0530

    pankaj on 14 Aug b4 merge

--- lib/common/base_url.dart
+++ lib/common/base_url.dart
@@ -4,26 +4,26 @@ class BaseUrl{
 
  ///UAT Server
 
-    static const String baseUrl = 'https://devapi.kanchaneshver.com/api/';
-   static const String imageBaseUrl = 'https://devksdpl-uploads.s3.ap-south-1.amazonaws.com/';
+   // static const String baseUrl = 'https://devapi.kanchaneshver.com/api/';
+   //static const String imageBaseUrl = 'https://devksdpl-uploads.s3.ap-south-1.amazonaws.com/';
 
  //  static const String baseUrl = 'https://devapi.kanchaneshver.com/api/';
 //   static const String imageBaseUrl = 'https://devksdpl-uploads.s3.ap-south-1.amazonaws.com/';
 
 
  ///Live Server
- // static const String baseUrl = 'https://api.kanchaneshver.com/api/';
+  static const String baseUrl = 'https://api.kanchaneshver.com/api/';
 
 
- //static const String imageBaseUrl = 'https://ksdpl-uploads.s3.ap-south-1.amazonaws.com/';
+ static const String imageBaseUrl = 'https://ksdpl-uploads.s3.ap-south-1.amazonaws.com/';
 
  //static const String imageBaseUrl = 'https://ksdpl-uploads.s3.ap-south-1.amazonaws.com/';
 
 
    static const String tutorialVideoBaseUrl = 'https://devksdpl-uploads.s3.ap-south-1.amazonaws.com/';
 
- // static const String devVersion = 'Dev App Version 1.0.22';
-   static const String devVersion = 'UAT';
+  static const String devVersion = 'Dev App Version 1.0.23';
+  // static const String devVersion = 'UAT';
  // static const String devVersion = 'Local';
 
   static const String buildDate = 'Build Date : 8 Aug 2025';
