2d5f89927db41b301149a05d1d41693c0578c0d9
Pankaj Devda <pankaj.devda@netspacesoftware.com>
2026-01-14 13:31:25 +0530

    multi-role committed by Pankaj on 14 Jan frst commit

--- lib/common/base_url.dart
+++ lib/common/base_url.dart
@@ -1,29 +1,34 @@
+
+///This is experiment-multi-role branch to make app avaiable for all upper level employees of KSDPL
+
 class BaseUrl{
   ///Local Server
   //static const String baseUrl = 'http://192.168.29.70:98/api/';
 
-  ///UAT Server
- static const String mainBaseUrl = 'https://devapi.kanchaneshver.com';
+  ///UAT Server experiment-multi-role
+/* static const String mainBaseUrl = 'https://devapi.kanchaneshver.com';
 static const String baseUrl = '$mainBaseUrl/api/';
 static const String imageBaseUrl = 'https://devksdpl-uploads.s3.ap-south-1.amazonaws.com/';
 static const String tutorialVideoBaseUrl = 'https://devksdpl-uploads.s3.ap-south-1.amazonaws.com/';
- static const String salesPanel = 'https://devsales.kanchaneshver.com';
+ static const String salesPanel = 'https://devsales.kanchaneshver.com';*/
 
-  ///Live Server
-/*
+  ///Live Server experiment-multi-role
  static const String mainBaseUrl = 'https://api.kanchaneshver.com';
  static const String baseUrl = '$mainBaseUrl/api/';
  static const String imageBaseUrl = 'https://ksdpl-uploads.s3.ap-south-1.amazonaws.com/';
  static const String tutorialVideoBaseUrl = 'https://ksdpl-uploads.s3.ap-south-1.amazonaws.com/';
  static const String salesPanel = 'https://sales.kanchaneshver.com';
-*/
 
 
   ///🚨⚠️ ALERT / WARNING
   ///Do change below devVersion also according to Live or UAT apk other wise it will give error in apk
- //static const String devVersion = 'LIVE';
-static const String devVersion = 'UAT';
+ static const String devVersion = 'LIVE';
+//static const String devVersion = 'UAT';
   //static const String devVersion = 'LOCAL';
-  static const String buildDate = 'Build Date : 9 Jan 2026';
+  static const String buildDate = 'Build Date : 13 Jan 2026';
+  static const String appType = 'Multi Role';
+  //static const String appType = 'RO/BM and AIC';
+
+
 }
 
